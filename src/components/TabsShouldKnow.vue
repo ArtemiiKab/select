46c5c36ea937430c4a2620ba9ab57c9ca31ce4af
Artemii_Kabanov <52568994+ArtemiiKab@users.noreply.github.com>
2020-10-12 12:27:53 +0300

    Update TabsShouldKnow.vue

--- src/components/TabsShouldKnow.vue
+++ src/components/TabsShouldKnow.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="row">
-	  <div class="large-12 columns text-center">
-			<h3><strong>What you should know</strong></h3>
-			<br>
-			<p><strong>Frame Condition</strong><br> Frames must be in good original condition and not liable to break during the glazing process. Frames should not be adapted or repainted in any way.</p>
-			<br>
-			<p><strong>Semi-Rimless/Rimless Frames</strong><br> Semi-Rimless/Rimless frames must have at least one non-broken lens in order to be reglazed.</p>
-			<br>
-			<p><strong>Curved Frames / Oversized Lenses</strong><br> Unfortunately, we're unable to reglaze frames or sunglasses with a curvature or large lenses. Please contact us if you're unsure if your frame is suitable.<br> <br> <strong>Varifocal/Progressive Lenses</strong><br> If you're choosing to reglaze a frame with varifocal/progressive lenses, please ensure the minimum height of your frame is 26mm.  Unfortunately we're unable to glaze a frame with varifocal lenses if the height is less than this.</p>
-			<br>
-			<p><strong>Risk</strong><br> All reglazes are done at the customers own risk.&nbsp;Ensure your frames are sent to us via an insured method, we cannot be held responsible for frames lost in the post.</p>
-			<br>
-			<p><em><strong><u>Please note</u></strong><br> While we make every effort to ensure all reglaze orders are completed, there are occasions where we're unable to complete the reglaze due to damage, brittle frames or a frame design that is not suitable for new lenses to be fitted successfully. In these cases we will contact you to advise you and return your frame and offer a refund. Unfortunately, we cannot refund postage fees.<br> <br> If you feel your frames may not be suitable for reglaze or would like advice, please contact us (include a picture of the frame if possible) and we'll be happy to assist you before you place your order.<br> <br> <strong>Lindberg Frames</strong><br> Unfortunately, due to the complexity of these frames, we're unable to provide a reglaze service for this brand.</em></p>
-			<br>
-		</div>
-	</div>
+  <div class="row"> 
+    <div class="large-12 columns text-center">
+      <h3><strong>What you should know</strong></h3>
+      <br>
+      <p><strong>Frame Condition</strong><br> Frames must be in good original condition and not liable to break during the glazing process. Frames should not be adapted or repainted in any way.</p>
+      <br>
+      <p><strong>Semi-Rimless/Rimless Frames</strong><br> Semi-Rimless/Rimless frames must have at least one non-broken lens in order to be reglazed.</p>
+      <br>
+      <p><strong>Curved Frames / Oversized Lenses</strong><br> Unfortunately, we're unable to reglaze frames or sunglasses with a curvature or large lenses. Please contact us if you're unsure if your frame is suitable.<br> <br> <strong>Varifocal/Progressive Lenses</strong><br> If you're choosing to reglaze a frame with varifocal/progressive lenses, please ensure the minimum height of your frame is 26mm.  Unfortunately we're unable to glaze a frame with varifocal lenses if the height is less than this.</p>
+      <br>
+      <p><strong>Risk</strong><br> All reglazes are done at the customers own risk.&nbsp;Ensure your frames are sent to us via an insured method, we cannot be held responsible for frames lost in the post.</p>
+      <br>
+      <p><em><strong><u>Please note</u></strong><br> While we make every effort to ensure all reglaze orders are completed, there are occasions where we're unable to complete the reglaze due to damage, brittle frames or a frame design that is not suitable for new lenses to be fitted successfully. In these cases we will contact you to advise you and return your frame and offer a refund. Unfortunately, we cannot refund postage fees.<br> <br> If you feel your frames may not be suitable for reglaze or would like advice, please contact us (include a picture of the frame if possible) and we'll be happy to assist you before you place your order.<br> <br> <strong>Lindberg Frames</strong><br> Unfortunately, due to the complexity of these frames, we're unable to provide a reglaze service for this brand.</em></p>
+      <br>
+    </div>
+  </div>
 </template>
