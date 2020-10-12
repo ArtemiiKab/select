e1802cb377948e4fa493ae7276f133a54f19f9c8
Artemii_Kabanov <52568994+ArtemiiKab@users.noreply.github.com>
2020-10-12 12:26:13 +0300

    Update TabSteps.vue

--- src/components/TabSteps.vue
+++ src/components/TabSteps.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="col-12 text-center">
-		<h3><strong>Replacing your lenses is easy </strong></h3>
-		<p><em>This is how it works...</em></p>
-		<br>
-		<div class="row">
-			<div class="col-lg-3 col-md-6">
-				<div class="reglazeHIWTile">
-					<p class="largest"><strong>Step 1</strong></p>
-					<p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step1.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step1.jpg?20201008144715" style="display: inline-block;"></p>
-					<p>Choose the frame style you want to reglaze.<br> <br> <em>(we can also reglaze sunglasses, with or without prescription lenses, but they must not have a curvature or oversized lenses - please see the "<strong>What you should know</strong>" section below or contact us for more information if required)</em></p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<div class="reglazeHIWTile">
-					<p class="largest"><strong>Step 2</strong></p>
-					<p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step2.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step2.jpg?20201008144715" style="display: inline-block;"></p>
-					<p>Enter your prescription and choose your lenses.<br> <br> <em>(For varifocals/progressives, please ensure the minimum height of your frame is 26mm)</em></p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<div class="reglazeHIWTile">
-					<p class="largest"><strong>Step 3</strong></p>
-					<p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step3.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step3.jpg?20201008144715" style="display: inline-block;"></p>
-					<p>Pay for your order and send your frames to us by insured post to:<br> <br> <strong>SelectSpecs.com</strong><br> Lockwoods Yard, The Grove, Westgate on Sea, Kent, UK. CT8 8AS.<br> <br> Please include your order number, name and address with your frame.</p>
-			  </div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<div class="reglazeHIWTile">
-					<p class="largest"><strong>Step 4</strong></p>
-					<p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step4.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step4.jpg?20201008144715" style="display: inline-block;"></p>
-					<p>We'll fit your new lenses, complete our <a href="/info/quality-control/" class="blue_ul">14 point quality check</a> and return your glasses to you within <strong>7-14</strong> days.<br> <br> <strong><u>Please note</u></strong><br> we can only return your frames via insured post or courier.</p>
-				</div>
-			</div>
+    <h3><strong>Replacing your lenses is easy </strong></h3>
+      <p><em>This is how it works...</em></p>
+      <br>
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+	  <div class="reglazeHIWTile">
+	    <p class="largest"><strong>Step 1</strong></p>
+	    <p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step1.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step1.jpg?20201008144715" style="display: inline-block;"></p>
+	    <p>Choose the frame style you want to reglaze.<br> <br> <em>(we can also reglaze sunglasses, with or without prescription lenses, but they must not have a curvature or oversized lenses - please see the "<strong>What you should know</strong>" section below or contact us for more information if required)</em></p>
+	  </div>
+        </div>
+	<div class="col-lg-3 col-md-6">
+	  <div class="reglazeHIWTile">
+	    <p class="largest"><strong>Step 2</strong></p>
+	    <p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step2.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step2.jpg?20201008144715" style="display: inline-block;"></p>
+	    <p>Enter your prescription and choose your lenses.<br> <br> <em>(For varifocals/progressives, please ensure the minimum height of your frame is 26mm)</em></p>
 	  </div>
 	</div>
+	<div class="col-lg-3 col-md-6">
+	  <div class="reglazeHIWTile">
+	    <p class="largest"><strong>Step 3</strong></p>
+	    <p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step3.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step3.jpg?20201008144715" style="display: inline-block;"></p>
+	    <p>Pay for your order and send your frames to us by insured post to:<br> <br> <strong>SelectSpecs.com</strong><br> Lockwoods Yard, The Grove, Westgate on Sea, Kent, UK. CT8 8AS.<br> <br> Please include your order number, name and address with your frame.</p>
+	  </div>
+        </div>
+	<div class="col-lg-3 col-md-6">
+	  <div class="reglazeHIWTile">
+	    <p class="largest"><strong>Step 4</strong></p>
+	    <p><img class="lazy" data-original="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step4.jpg?20201008144715" src="https://d9qzjwuieyamt.cloudfront.net/5.4.2.7/res/images/icons/reglaze/step4.jpg?20201008144715" style="display: inline-block;"></p>
+	    <p>We'll fit your new lenses, complete our <a href="/info/quality-control/" class="blue_ul">14 point quality check</a> and return your glasses to you within <strong>7-14</strong> days.<br> <br> <strong><u>Please note</u></strong><br> we can only return your frames via insured post or courier.</p>
+	  </div>
+       </div>
+     </div>
+  </div>
 </template>
